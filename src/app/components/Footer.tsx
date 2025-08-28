45c9fc7ca5938b0b81df112c134b86144e23f82a
Lee Egino <lee@invoco.net>
2025-08-29 07:13:23 +0800

    Update Footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,6 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-8">
       <div className="container mx-auto text-center text-sm">
         <p className="mb-2">© {new Date().getFullYear()} Teacher Joy. All Rights Reserved.</p>
-        <p>Designed and Built with Next.js and Tailwind CSS.</p>
       </div>
     </footer>
   );
